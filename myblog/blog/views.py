c6afc75942d436fc6a8be523835f557e803f71e2
许哲 <592717159@qq.com>
2017-04-06 22:22:40 +0800

    20170406

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from blog.models import Article
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'blog/index.html', {'article': Article.objects.all()})
+    return render(request, 'blog/index.html',
+                  {'article': Article.objects.all()})
 
 
 def article_getpage(request, article_id):
-    return render(request, 'blog/page.html',{'article': Article.objects.get(id=article_id)})
+    return render(request, 'blog/page.html',
+                  {'article': Article.objects.get(id=article_id)})
 
 
-def edit_page(request,article_id):
-    if str(article_id)=='0':
+def edit_page(request, article_id):
+    if str(article_id) == '0':
         return render(request, 'blog/edit_page.html')
-    return render(request, 'blog/edit_page.html',{'article': Article.objects.get(id=article_id)})
+    return render(request, 'blog/edit_page.html',
+                  {'article': Article.objects.get(id=article_id)})
 
 
 def edit_page_action(request):
@@ -25,7 +28,7 @@ def edit_page_action(request):
     content = request.POST.get('content', 'CONTENT')
     if content == "":
         content = 'content'
-    article_id = request.POST.get('article_id','0')
+    article_id = request.POST.get('article_id', '0')
     if str(article_id) == '0':
         Article.objects.create(title=title, content=content)
         return HttpResponseRedirect('/index/index')
@@ -33,8 +36,9 @@ def edit_page_action(request):
     article.title = title
     article.content = content
     article.save()
-    return HttpResponseRedirect('/index/pages/%d'%article.id)
+    return HttpResponseRedirect('/index/pages/%d' % article.id)
 
-def page_delete(request,article_id):
+
+def page_delete(request, article_id):
     Article.objects.get(id=article_id).delete()
     return HttpResponseRedirect('/index/index')
